8d899b757019f4a7f0de4dc235b5c5ba3a9e3ccc
trozic <rozic.tajana@gmail.com>
2016-04-04 18:22:00 +0200

    Settings commit

--- elearning_red/elearning_red/settings.py
+++ elearning_red/elearning_red/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'elearning.apps.ElearningConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'elearning_red.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'elearning_mysql',
+        'USER': 'root',
+        'PASSWORD': 'rokerica',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
