de2b3164fdd4f7086eee3de0d778e587e6827bc5
IvanIvan353 <blalba353@gmail.com>
2016-04-06 23:15:39 +0200

    kreiranje usera koji za sad nemaju role

--- elearning_red/elearning/views.py
+++ elearning_red/elearning/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, Http404
+from django.contrib.auth import login
 
-# Create your views here.
+from forms import UserForm
+from .models import CustomUser
+from .models import Role
+
+def registration(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_student = CustomUser.objects.create_user(**form.cleaned_data)
+  
+	  # role = Role.objects.get(name='student') 
+	  # role.entry_set.add(new_student)
+	    
+	  # login(new_student)
+            
+            return HttpResponseRedirect('')
+    else:
+        form = UserForm() 
+
+    return render(request, 'registration.html', {'form': form}) 
+
