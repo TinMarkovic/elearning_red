826c2acf3e60a28f2c55f68d36389278b8e47f25
hw-tin <tinmarkoviccs@gmail.com>
2016-04-08 22:48:00 +0200

    Removed some sideline comments

--- elearning_red/elearning/forms.py
+++ elearning_red/elearning/forms.py
@@ -14,24 +14,3 @@ class CourseForm(ModelForm):
         widgets = {
             'beginDate': widgets.SelectDateWidget(),
         }
-
-""" 
-class CreateStudentForm(forms.ModelForm):
-    class Meta(object):
-        model = Student
-        fields = ('first_name', 'last_name', 'age', 'about')
-
-    def clean_age(self):
-        age = self.cleaned_data.get('age')
-        if age and age > 100:
-            raise forms.ValidationError("Too old student")
-        return age
-
-    def clean(self):
-        cleaned_data = super(CreateStudentForm, self).clean()
-        first_name = cleaned_data.get('first_name')
-        last_name = cleaned_data.get('last_name')
-        if first_name and last_name and first_name == last_name:
-            raise forms.ValidationError("First and last names cannot be the same!")
-        return cleaned_data
-"""
