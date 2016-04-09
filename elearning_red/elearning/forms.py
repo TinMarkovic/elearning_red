7d084a21c0ddc7f6a8d94eb77141a8f2265a9aa5
hw-tin <tinmarkoviccs@gmail.com>
2016-04-09 10:27:30 +0200

    Added widget to User Model, redefined models in forms

--- elearning_red/elearning/forms.py
+++ elearning_red/elearning/forms.py
@@ -1,15 +1,17 @@
-from datetime import date
 from django.forms import ModelForm, widgets
-from .models import CustomUser, Course
+import models as M
 
 class UserForm(ModelForm):
     class Meta:
-        model = CustomUser
+        model = M.CustomUser
         fields = ('username', 'first_name', 'last_name', 'email', 'password', 'dob')
+        widgets = {
+            'dob': widgets.SelectDateWidget(),
+        }
         
 class CourseForm(ModelForm):
     class Meta:
-        model = Course
+        model = M.Course
         fields = ('name', 'desc', 'beginDate', 'duration', 'author', 'tags')
         widgets = {
             'beginDate': widgets.SelectDateWidget(),
