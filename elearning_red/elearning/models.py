aa35fab45855c228c1d2c129ef0af4e756946fc3
hw-tin <tinmarkoviccs@gmail.com>
2016-04-29 17:36:46 +0200

    Refactored URLs and Models somewhat - TODO: update hardcoded (possibly remove them) URLs

--- elearning_red/elearning/models.py
+++ elearning_red/elearning/models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
 from django.db import models
-from django.contrib.auth.models import models
 from django.contrib.auth.models import User
-from django.utils.text import slugify
-from django.db.models.signals import pre_save
+from django.utils.functional import cached_property
+# Commented out for now. Didn't see them used anywhere:
+#from django.db.models.signals import pre_save
+#from django.utils.text import slugify
+#from django.contrib.auth.models import models
 
 class Permission(models.Model):
     name = models.CharField(max_length=25, blank=False, null=False)
@@ -13,7 +15,7 @@ class Permission(models.Model):
    
 class Role(models.Model):
     name = models.CharField(max_length=25, blank=False, null=False)
-    permissions = models.ManyToManyField(Permission)
+    permissions = models.ManyToManyField(Permission, blank=True)
     
     def __unicode__(self):
         return self.name
@@ -21,6 +23,11 @@ class Role(models.Model):
 class CustomUser(User):
     dob = models.DateField()
     role = models.ForeignKey(Role, on_delete=models.CASCADE, blank=True, null=True)
+    
+    @cached_property
+    def getRole(self):
+        userRole = self.role.name;
+        return userRole
     
     def __unicode__(self):
         return self.first_name + ' ' + self.last_name
@@ -35,23 +42,23 @@ class Tag(models.Model):
 class Programme(models.Model):
     name = models.CharField(max_length=40, blank=False, null=False)
     desc = models.TextField()
-    avgRating = models.DecimalField(max_digits=5,decimal_places=2)
-    tags = models.ManyToManyField(Tag)
-    users = models.ManyToManyField(CustomUser)
+    avgRating = models.DecimalField(max_digits=5,decimal_places=2, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    users = models.ManyToManyField(CustomUser, blank=True)
     
     def __unicode__(self):
         return self.name
 
 class Course(models.Model):
     name = models.CharField(max_length=40, blank=False, null=False)
-    desc = models.TextField()
-    beginDate = models.DateField()
-    duration = models.PositiveSmallIntegerField()
-    author = models.TextField()
-    tags = models.ManyToManyField(Tag)
-    users = models.ManyToManyField(CustomUser)
-    programmes = models.ManyToManyField(Programme)
-    avgRating = models.DecimalField(max_digits=5,decimal_places=2)
+    desc = models.TextField(blank=True, null=True)
+    beginDate = models.DateField(blank=True, null=True)
+    duration = models.PositiveSmallIntegerField(blank=True, null=True)
+    author = models.TextField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    users = models.ManyToManyField(CustomUser, blank=True)
+    programmes = models.ManyToManyField(Programme, blank=True)
+    avgRating = models.DecimalField(max_digits=5, decimal_places=2, default=0, blank=True, null=True)
     
     
     def __unicode__(self):
@@ -59,8 +66,8 @@ class Course(models.Model):
     
 class Section(models.Model):
     name = models.CharField(max_length=40, blank=False, null=False)
-    desc = models.TextField()
-    beginDate = models.DateField()
+    desc = models.TextField(blank=True, null=True)
+    beginDate = models.DateField(blank=True, null=True)
     index = models.PositiveSmallIntegerField(blank=False, null=False)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     
@@ -71,7 +78,7 @@ class Section(models.Model):
 class Block(models.Model): # Generic block model
     name = models.CharField(max_length=40, blank=False, null=False)
     index = models.PositiveSmallIntegerField(blank=False, null=False)
-    assessment = models.BooleanField()
+    assessment = models.BooleanField(default=False)
     sections = models.ForeignKey(Section, on_delete=models.CASCADE)
     
     def __unicode__(self):
@@ -87,8 +94,8 @@ class VideoBlock(Block):
     url = models.URLField(max_length=250, blank=False, null=False)
 
 class ImageBlock(Block):
-    subtitle = models.CharField(max_length=40, blank=False, null=False)
-    image = models.ImageField(upload_to="", blank=True, null=True)
+    subtitle = models.CharField(max_length=40, blank=True, null=True)
+    image = models.ImageField(upload_to="", blank=False, null=False)
 
 class QuizBlock(Block):
     serialQuestions = models.TextField(blank=False, null=False)
